cf1fa94c802f6fd8514f52cd8536e3260fdc46c5
Sivashankar <sravisa1@volvocars.com>
2022-12-27 18:58:00 +0100

    Add files via upload

---
+++ variables/src/main/java/VarType.java
@@ -0,0 +1,10 @@
+public class VarType {
+    // var myName = "OCP17"; // does not compile
+
+    public static void main(String[] args) {
+        var myName = "OCP17";
+        var myVersion = 17;
+
+        System.out.println("Exercise for var type");
+    }
+}
